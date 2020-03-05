f58c0556d1d60ab8a2050ece19445b6390554fdc
Gev Shahnazaryan <shgev555@gmail.com>
2020-03-05 23:58:33 +0400

    Changed for last version of WP

--- templates/settings.php
+++ templates/settings.php
@@ -1,8 +1,8 @@
 <div class="wrap">
     <h2>Job Seeker Settings</h2>
     <form method="post" action="options.php"> 
-        <?php @settings_fields('wp_job_seeker-group'); ?>
-        <?php @do_settings_fields('wp_job_seeker-group'); ?>
+        <?php @settings_fields('wp_job_seeker','wp_job_seeker-group'); ?>
+        <?php @do_settings_fields('wp_job_seeker','wp_job_seeker-group'); ?>
 
         <?php do_settings_sections('wp_job_seeker'); ?>
 
